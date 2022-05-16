06bcceff7b45a549019780539147a747adea40b4
SHIM <weasd33@naver.com>
2022-05-16 20:56:36 +0900

    스프링 DI 지시서 작성하기(Spring Bean Configuration)

--- spring/src/spring/di/Program.java
+++ spring/src/spring/di/Program.java
@@ -9,10 +9,17 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		/* 스프링에게 지시하는 방법으로 코드를 변경
 		Exam exam = new NewlecExam();
-//		ExamConsole console = new InlineExamConsole(exam); // DI
-		ExamConsole console = new GridExamConsole(exam);
-		console.print();
+		ExamConsole console = new GridExamConsole();
+		
+		console.setExam(exam);
+		*/
+		
+		
+		
+//		ExamConsole console = ?;
+//		console.print();
 	}
 
 }
--- spring/src/spring/di/ui/InlineExamConsole.java
+++ spring/src/spring/di/ui/InlineExamConsole.java
@@ -6,6 +6,8 @@ public class InlineExamConsole implements ExamConsole {
 
 	private Exam exam;
 	
+	public InlineExamConsole() {}
+	
 	public InlineExamConsole(Exam exam) {
 		this.exam = exam;
 	}
@@ -13,6 +15,11 @@ public class InlineExamConsole implements ExamConsole {
 	@Override
 	public void print() {
 		System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
+	}
+
+	@Override
+	public void setExam(Exam exam) {
+		this.exam = exam;
 	}
 
 }
