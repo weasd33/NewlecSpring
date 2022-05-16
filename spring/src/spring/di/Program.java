90033f006bedc56e97aad4eed7a5be3c5cd09a89
SHIM <weasd33@naver.com>
2022-05-16 17:52:47 +0900

    Dependency를 직접 Injection하기

---
+++ spring/src/spring/di/Program.java
@@ -0,0 +1,18 @@
+package spring.di;
+
+import spring.di.entity.Exam;
+import spring.di.entity.NewlecExam;
+import spring.di.ui.ExamConsole;
+import spring.di.ui.*;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Exam exam = new NewlecExam();
+//		ExamConsole console = new InlineExamConsole(exam); // DI
+		ExamConsole console = new GridExamConsole(exam);
+		console.print();
+	}
+
+}
