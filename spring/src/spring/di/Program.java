69bf5c72b6ecc97861574957d6bc2b8f6ef2515b
SHIM <weasd33@naver.com>
2022-05-26 14:23:09 +0900

    콜렉션 생성과 목록 DI

--- spring/src/spring/di/Program.java
+++ spring/src/spring/di/Program.java
@@ -1,9 +1,13 @@
 package spring.di;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import spring.di.entity.Exam;
+import spring.di.entity.NewlecExam;
 import spring.di.ui.ExamConsole;
 
 public class Program {
@@ -24,6 +28,13 @@ public class Program {
 //		ExamConsole console = (ExamConsole) context.getBean("console");
 		ExamConsole console = context.getBean(ExamConsole.class);
 		console.print();
+		
+		List<Exam> exams = (List<Exam>) context.getBean("exams"); //new ArrayList<>();
+		exams.add(new NewlecExam(1,1,1,1));
+		
+		for(Exam e : exams) {
+			System.out.println(e);
+		}
 	}
 
 }
