090a315176f6d9e629b50ee3d1b25a47e390c554
SHIM <weasd33@naver.com>
2022-06-09 01:12:45 +0900

    After Returning / Throwing 어드바이스 구현하기

--- spring/src/spring/aop/entity/NewlecExam.java
+++ spring/src/spring/aop/entity/NewlecExam.java
@@ -46,6 +46,9 @@ public class NewlecExam implements Exam {
 		
 		int result = kor + eng + math + com;
 		
+		if(kor > 100)
+			throw new IllegalArgumentException("유효하지 않은 국어점수");
+		
 		try {
 			Thread.sleep(200);
 		} catch (InterruptedException e) {
