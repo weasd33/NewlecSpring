10b639e7f18e6b840e31d95d1a22295526a4f333
SHIM <weasd33@naver.com>
2022-06-09 23:08:18 +0900

    공지사항 컨트롤러 추가하기

--- webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
+++ webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
@@ -1,4 +1,4 @@
-package com.newlecture.web.controller;
+package com.newlecture.web.controller.notice;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -6,16 +6,15 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-public class IndexController implements Controller {
+public class ListController implements Controller {
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-		ModelAndView mv = new ModelAndView("index");
-		mv.addObject("data", "Hello Spring MVC");
+		ModelAndView mv = new ModelAndView("notice/list");
 		//mv.setViewName("/WEB-INF/view/index.jsp");
 		
 		return mv;
 	}
-	
+
 }
