4e0f54102b056f2bf5b98e859b958ad87754b3cb
SHIM <weasd33@naver.com>
2022-06-10 22:13:31 +0900

    서비스 객체 분리하기

--- webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
+++ webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.mvc.Controller;
 
 import com.newlecture.web.entity.Notice;
 import com.newlecture.web.service.NoticeService;
+import com.newlecture.web.service.jdbc.JDBCNoticeService;
 
 public class ListController implements Controller {
 
