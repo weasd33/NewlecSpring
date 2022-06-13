c07b7ce7696fb9c11328ac4b7492968944199b54
SHIM <weasd33@naver.com>
2022-06-13 11:57:10 +0900

    객체 DI를 Annotaion으로 변경하기

--- webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
+++ webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -14,12 +15,9 @@ import com.newlecture.web.service.jdbc.JDBCNoticeService;
 
 public class ListController implements Controller {
 
+	@Autowired
 	private NoticeService noticeService;
 	
-	public void setNoticeService(NoticeService noticeService) {
-		this.noticeService = noticeService;
-	}
-
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
--- webprj/src/main/java/com/newlecture/web/service/jdbc/JDBCNoticeService.java
+++ webprj/src/main/java/com/newlecture/web/service/jdbc/JDBCNoticeService.java
@@ -12,6 +12,8 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.newlecture.web.entity.Notice;
 import com.newlecture.web.service.NoticeService;
 
@@ -21,11 +23,8 @@ public class JDBCNoticeService implements NoticeService {
 //	private String pwd = "1234";
 //	private String driver = "oracle.jdbc.driver.OracleDriver";
 	
+	@Autowired
 	private DataSource dataSource;
-	
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
-	}
 
 	public List<Notice> getList(int page, String field, String query) throws ClassNotFoundException, SQLException {
 
