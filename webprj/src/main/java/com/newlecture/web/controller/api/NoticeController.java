1533dede80db721c6d5fbe009b0b201b43d0850e
SHIM <weasd33@naver.com>
2022-06-14 14:23:24 +0900

    QueryString 입력 : 변수명 별칭과 기본 값처리

--- webprj/src/main/java/com/newlecture/web/controller/api/NoticeController.java
+++ webprj/src/main/java/com/newlecture/web/controller/api/NoticeController.java
@@ -3,6 +3,8 @@ package com.newlecture.web.controller.api;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- webprj/src/main/java/com/newlecture/web/controller/customer/NoticeController.java
+++ webprj/src/main/java/com/newlecture/web/controller/customer/NoticeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.newlecture.web.entity.Notice;
 import com.newlecture.web.service.NoticeService;
@@ -18,9 +19,11 @@ public class NoticeController {
 	private NoticeService noticeService;
 	
 	@RequestMapping("list")
-	public String list() throws ClassNotFoundException, SQLException {
+	public String list(@RequestParam(name = "p", defaultValue = "1") int page) throws ClassNotFoundException, SQLException {
+//		String p = request.getParameter("p");
+		System.out.println("page : " + page);
 		
-		List<Notice> list = noticeService.getList(1, "TITLE", "");
+//		List<Notice> list = noticeService.getList(1, "TITLE", "");
 		
 		return "notice.list";
 	}
