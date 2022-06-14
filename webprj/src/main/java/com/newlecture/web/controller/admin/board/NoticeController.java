225b98fcbd5d34f41a1b6f98d83c6a33f5e76a5e
SHIM <weasd33@naver.com>
2022-06-14 16:32:10 +0900

    POST 입력 #5(파일 업로드 설정하기)

--- webprj/src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
+++ webprj/src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
@@ -3,6 +3,7 @@ package com.newlecture.web.controller.admin.board;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 // 동일명의 클래스가 있기 때문에 컨트롤러에 명명해 줘야 된다.
 @Controller("adminNoticeController")
@@ -17,7 +18,11 @@ public class NoticeController { // <bean name="adminNoticeController" class="com
 
 	@RequestMapping("reg")
 	@ResponseBody
-	public String reg(String title, String content, String category, String[] foods, String food) {
+	public String reg(String title, String content, MultipartFile file, String category, String[] foods, String food) {
+		String fileName = file.getOriginalFilename();
+		long size = file.getSize();
+		System.out.printf("파일명:%s, 파일크기:%d\n", fileName, size);
+		
 		System.out.println(category);
 		for(String f : foods)
 			System.out.println(f);
